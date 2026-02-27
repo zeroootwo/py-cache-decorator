50e74c6b1658fc512669b8187c8444780570cef4
Anastasiia Velikaya <a.v.zaychenko@gmail.com>
2026-02-27 21:59:45 +0200

    Fix cache decorator

--- app/main.py
+++ app/main.py
@@ -5,11 +5,12 @@ def cache(func: Callable) -> Callable:
     cache_dict = {}
 
     def wrapper(*args: Any, **kwargs: Any) -> Any:
-        if args in cache_dict:
+        key = (args, tuple(sorted(kwargs.items())))
+        if key in cache_dict:
             print("Getting from cache")
-            return cache_dict[args]
+            return cache_dict[key]
         print("Calculating new result")
         result = func(*args, **kwargs)
-        cache_dict[args] = result
+        cache_dict[key] = result
         return result
     return wrapper
